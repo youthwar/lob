fb9525fe8a11143f3eb8a507902df7172aefe2ec
Maximiliano Ayala <max@youthwar.com>
2021-09-07 10:47:25 -0700

    one more cleanup

--- api/src/server.js
+++ api/src/server.js
@@ -13,18 +13,6 @@ app.get('/',(req,res) => {
 
 app.use('/address', address);
 
-/* app.get('/search', (req, res) => {
-  const { query: { query } } = req;
-  const isEmpty = query.length ? query.replace(/\s/g,"") === '' : true;
-  if (!isEmpty) {
-    const results = search.query(query);
-    return res.send({ results })
-  } else {
-    return res.status(404).send({ message: 'uhoh'})
-  }
-});
-*/
-
 app.listen(PORT,() => {
      console.log(`app is listening to port ${PORT}`);
 });
